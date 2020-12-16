a85b071f93ed61cf60f39adea73818287f747995
del254 <75515822+del254@users.noreply.github.com>
2020-12-16 13:17:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from "react"
+import Record from "./fetchrecords";
+import './App.css'
 
-function App() {
+const App = () => {
+   
+  const searchcategory = "food";
+   
+  const category = 'https://api.chucknorris.io/jokes/random?category={searchcategory}';
+  const [records, getrecords] = useState(['']);
+  const [search, getsearch] = useState("");
+  useEffect(() => {
+    getitems();
+  }, []);
+  const getitems = async () => {
+    const response = await fetch (`https://api.chucknorris.io/jokes/random?category=${searchcategory}`);
+    const data = await response.json();
+    console.log(data);
+    getrecords(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="maincontent">
+       <form action="" method="post">
+         <center><span>search here by category </span></center>
+         <hr/>
+         <input type="text" className="inputbox" value={search} />
+         <button type="submit" className="submitbtn">Request Data</button>
+       </form>
+
+        
+       <div className="results">
+         <Record  idnumber={records.id} iconurl={records.icon_url} value={records.value} dateinserted={records.created_at} dateupdated={records.created_at} />
+        
+       </div>
+     </div>
   );
 }
-
 export default App;
+
